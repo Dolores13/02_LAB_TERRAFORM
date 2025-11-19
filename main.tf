c253163c0d655b7274aea85b3a2f01b7e69d09c7
L00196725 <L00196725@atu.ie>
2025-11-19 10:36:19 +0000

    Security Group port 22 and 80

--- main.tf
+++ main.tf
@@ -94,3 +94,37 @@ module "vpc" {
   }
 }
 
+# Security Group: 22,80
+resource "aws_security_group" "web_sg" {
+  name        = "${local.project}-web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name     = "${local.project}-web-sg"
+    extraTag = local.extra_tag
+  }
+}
