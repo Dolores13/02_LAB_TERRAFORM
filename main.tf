ea1db580e35963f2b12a0fe1d516887014e441fc
L00196725 <L00196725@atu.ie>
2025-11-19 01:15:35 +0000

    module vpc

--- main.tf
+++ main.tf
@@ -46,6 +46,11 @@ variable "ami_id" {
   default     = "ami-054d6a336762e438e"
 }
 
+locals {
+  project   = "lab2-terraform"
+  extra_tag = "extra-tag"
+}
+
 
 # Providers
 
@@ -53,5 +58,39 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+}
+
+# VPC (2 zones , 2 public, 2 private)
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.8"
+ 
+
+  name = "${local.project}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs              = ["${var.region}a", "${var.region}b"]
+  public_subnets   = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets  = ["10.0.10.0/24", "10.0.20.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Project     = local.project
+    Environment = "dev"
+  }
+   public_subnet_tags = {
+    "kubernetes.io/cluster/${local.project}-eks" = "shared"
+    "kubernetes.io/role/elb"                     = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.project}-eks" = "shared"
+    "kubernetes.io/role/internal-elb"            = "1"
+  }
 }
 
